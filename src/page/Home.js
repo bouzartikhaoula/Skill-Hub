a8bd9ab8f071345b3ded2a37519c7e2bf3e29e2b
khaoula <bouzartikhaoula@gmail.com>
2024-08-24 14:27:38 +0200

    new section

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Image, Row } from "react-bootstrap";
 import image1 from "../img/imag1.png";
 import image2 from "../img/image2.png";
+import image3 from "../img/image3.png";
+import iconFolder from "../img/iconFolder.png"
 
 const Home = () => {
   return (
     <Container>
+      {/* section 1 */}
       <div className="row pt-3">
         <div className="col-lg-6">
           <h3 className="greentext mb-4">Grow yourself with us</h3>
@@ -34,6 +37,7 @@ const Home = () => {
           <Image className="w-100 " src={image1} />
         </div>
       </div>
+      {/* section 2 */}
       <div className="row pb-4">
         <div className="col-lg-6">
           <Image className="w-100 " src={image2} />
@@ -43,15 +47,100 @@ const Home = () => {
             Over 1M+ Students get’s job
           </h1>
           <p className="mb-4" style={{ fontSize: "20px" }}>
-          Non-disclosure agreement seed round seed money accelerator influencer. Growth hacking return nondis sure .
+            Non-disclosure agreement seed round seed money accelerator
+            influencer. Growth hacking return nondis sure.
           </p>
           <p className="mb-4" style={{ fontSize: "20px" }}>
-          Non-disclosure agreement seed round seed money accelerator influencer.
+            Non-disclosure agreement seed round seed money accelerator
+            influencer.
           </p>
-          <Button className="p-3 pe-5 ps-5" type="submit">Explore More</Button>
-
+          <Button className="p-3 pe-5 ps-5" type="submit">
+            Explore More
+          </Button>
         </div>
       </div>
+      {/* section 3 */}
+      <section>
+       <div className="text-center mt-5" >
+       <h1 style={{fontSize:" 60px"}}>How it works</h1>
+       <p style={{fontSize:" 20px"}}>Non-disclosure agreement seed round seed money <br/> accelerator influencer.</p>
+       </div>
+       <Row className="pt-5 mt-5">
+        <Col>
+        <Card className="startCard" >
+       
+      <Card.Body>
+       <Row>
+<Col xs={1}>
+<Card.Img className="icon pt-2"  src={iconFolder} />
+
+</Col>       
+        
+       
+       <Col>
+       <Card.Title style={{fontSize: "40px"}}>Select a course
+       </Card.Title>
+        <Card.Text className="pt-3 pb-3">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       </Col>
+        </Row>
+
+      </Card.Body>
+        </Card>
+
+        <Card className="mt-4" >
+       
+      <Card.Body>
+       <Row>
+<Col xs={1}>
+<Card.Img className="icon pt-2"  src={iconFolder} />
+
+</Col>       
+        
+       
+       <Col>
+       <Card.Title style={{fontSize: "40px"}}>Select a course
+       </Card.Title>
+        <Card.Text className="pt-3 pb-3">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       </Col>
+        </Row>
+
+      </Card.Body>
+    </Card>
+    <Card className="mt-4" >
+       
+      <Card.Body>
+       <Row>
+<Col xs={1}>
+<Card.Img className="icon pt-2"  src={iconFolder} />
+
+</Col>       
+        
+       
+       <Col>
+       <Card.Title style={{fontSize: "40px"}}>Select a course
+       </Card.Title>
+        <Card.Text className="pt-3 pb-3">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       </Col>
+        </Row>
+
+      </Card.Body>
+    </Card>
+
+        </Col>
+        <Col>
+        <Image className="w-100" src={image3}/>
+        </Col>
+       </Row>
+      </section>
     </Container>
   );
 };
