aad864ce450590cb0b7c78fed980e43f77f03228
khaoula <bouzartikhaoula@gmail.com>
2024-08-28 19:49:32 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Courses from './page/Courses'
 import OnlineLearning from './page/OnlineLearning'
 import Resources from './page/Resources'
 import Footer from './component/Footer'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 
 const App = () => {
   return (
     <>
+
       <Navbartop/>
       <Routes>
 
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,26 +1,104 @@
 import React from "react";
 import { Col, Container, Image, Placeholder, Row } from "react-bootstrap";
-import logo from "../img/logw.png"
+import logo from "../img/logw.png";
 const Footer = () => {
   return (
-    <Container className="bg-dark text-white"  >
-    <Row>
-    <Col xs={12} md={6}>
-        <Image src={logo} />
-        <p>Non-disclosure agreement seed round seed money accelerator influencer. Growth hacking return nondis sure agreement .</p>
-        <div className="d-flex">
-        <i class="bi bi-facebook">hh</i>
-        </div>
+    <div className="p-4 bg-dark text-white">
+      <Row>
+        <Col className="pb-4"  xs={12} md={5}>
+          <Image src={logo} className="mb-3" />
+          <p style={{ fontSize: "20px" }}>
+            Non-disclosure agreement seed round seed money accelerator
+            influencer. Growth hacking return nondis sure agreement .
+          </p>
+          <div className="d-flex justify-content-evenly">
+            <i class="bi bi-facebook "></i>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-linkedin"></i>
+            <i class="bi bi-twitter-x"></i>
+          </div>
         </Col>
-        <Col xs={12} md={3}>
-          xs=6 md=4
+       <Col >
+       <Row >
+       <Col  className="pb-4" xs={12} md={4} >
+          <h5 className="mb-4">About Production</h5>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Patch
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Updates
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Beta test
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Pricing Product
+          </a>
         </Col>
-        <Col xs={6} md={3}>
-          xs=6 md=4
+        <Col className="pb-4" xs={12} md={4} >
+          <h5 className="mb-4">Help and Solution</h5>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Talk to support
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Support docs
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            System status
+          </a>
         </Col>
-    </Row>
-    </Container>
-    
+        <Col className="pb-4" xs={12} md={4} >
+          <h5 className="mb-4">Support</h5>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Help center
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Account information
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="list-group-item list-group-item-action text-secondary mb-2 fs-5"
+          >
+            Contact Us
+          </a>
+        </Col>{" "}
+       </Row>
+       </Col>
+      </Row>
+    </div>
   );
 };
 
